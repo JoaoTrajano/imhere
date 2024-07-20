568ea39ca36adde813702dfa7ad7ae281072ad6a
João Trajano <joao.trajanosouza@gmail.com>
2024-07-20 16:08:37 -0300

    feature(home): adjusted buttons for add paricipant

--- components/TextInput/index.tsx
+++ components/TextInput/index.tsx
@@ -1,7 +1,17 @@
 import { TextInput as TextInputReactNative } from 'react-native';
+import { styles } from './style';
 
-export function TextInput() {
+type TextInputProps = {
+  handleFunction: (data: string) => void;
+  currentValue: string;
+};
+
+export function TextInput({ handleFunction, currentValue }: TextInputProps) {
   return (
-    <TextInputReactNative style={{ height: 40, backgroundColor: '#FFF' }} />
+    <TextInputReactNative
+      onChangeText={handleFunction}
+      value={currentValue}
+      style={styles.textInput}
+    />
   );
 }
--- components/screens/Home/index.tsx
+++ components/screens/Home/index.tsx
@@ -1,13 +1,27 @@
 import { View, Text } from 'react-native';
 import { styles } from './style';
 import { TextInput } from '@/components/TextInput';
+import { Button } from '@/components/Button';
+import { useState } from 'react';
 
 export function Home() {
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
+
   return (
     <View>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>20 de Julho de 2024</Text>
-      <TextInput />
+      <TextInput
+        currentValue={participantName}
+        handleFunction={setParticipantName}
+      />
+      <Button
+        content="+"
+        handleFunction={() =>
+          setParticipants((prevState) => [...prevState, participantName])
+        }
+      />
     </View>
   );
 }
